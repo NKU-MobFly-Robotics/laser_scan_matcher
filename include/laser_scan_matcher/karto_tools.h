85aedfb663851c75b5dcd642e3c14993ecb0788f
nkuwenjian <415664739@qq.com>
2020-02-05 15:26:09 +0800

    Add copyright

--- include/laser_scan_matcher/karto_tools.h
+++ include/laser_scan_matcher/karto_tools.h
@@ -1,7 +1,22 @@
-#ifndef LASER_SCAN_MATCHER_KARTO_TOOLS_H_
-#define LASER_SCAN_MATCHER_KARTO_TOOLS_H_
+/*
+ * Copyright 2010 SRI International
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#include <laser_scan_matcher/Math.h>
+#ifndef LASER_SCAN_MATCHER_KARTO_TOOLS_H
+#define LASER_SCAN_MATCHER_KARTO_TOOLS_H
 
 #include <algorithm>
 #include <fstream>
@@ -21,6 +36,8 @@
 #include <string.h>
 #include <boost/thread.hpp>
 #include <cstdlib>
+
+#include "laser_scan_matcher/Math.h"
 
 namespace scan_tools {
 /**
@@ -2688,4 +2705,4 @@ class ScanManager {
 
 }  // namespace scan_tools
 
-#endif  // LASER_SCAN_MATCHER_KARTO_TOOLS_H_
+#endif  // LASER_SCAN_MATCHER_KARTO_TOOLS_H
